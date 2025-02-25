d525a80a36c6985be9a9fb62d9f472cbf466d816
testaccjshtml <muzclouduz@gmail.com>
2025-02-26 00:52:22 +0500

    Add files via upload

---
+++ jsm.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+  setTimeout(function() {
+    let q=document.querySelector('.noty');
+    let w=document.querySelector('.noty__body');
+    let p=document.querySelector('.noty__text');
+    p.style.setProperty('display', 'none', 'important');
+    w.style.setProperty('display', 'none', 'important');
+    q.style.setProperty('display', 'none', 'important');
+  console.log("hello Raymov M.");
+  }, 15000);
+})();
